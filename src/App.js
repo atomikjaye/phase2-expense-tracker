b0e2d3c6f07aa19dc15461a633bb6b88a6a1efed
Jocelyn Naarden <tzionone@gmail.com>
2022-06-29 08:08:03 -0400

    Added Navigation/Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 // import logo from './logo.svg';
 import { Container } from 'semantic-ui-react'
+import { Routes, Route } from 'react-router-dom'
 import Dashboard from './components/DashBoard/Dashboard';
+import TransactionPage from './components/Transactions/TransactionPage'
+import Navbar from './components/NavBar/Navbar';
+import Form from './components/Form/Form'
 import trackData from "./db.json"
 import './App.css';
 
 function App() {
   return (
-    <Container fluid>
-      <Dashboard data={trackData} />
-    </Container>
+    <>
+      <Navbar />
+      <Container fluid>
+        <Routes>
+          <Route path="/" element={<Dashboard data={trackData} />} />
+          <Route path="transactions" element={<TransactionPage />} />
+          <Route path="form" element={<Form />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
